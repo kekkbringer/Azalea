f0514b6b8f6b3dd10f3c3d1ca2bb8fec59a9ed81
steinmet <dominik.steinmetz@kit.edu>
2023-08-07 19:50:37 +0200

    added another fen test

--- TESTS/board_test.cpp
+++ TESTS/board_test.cpp
@@ -199,4 +199,153 @@ TEST_CASE("Testing board representation...", "[board]") {
 	REQUIRE(b.whiteKing == Piece(Color::white, PieceType::king, 95));
 	REQUIRE(b.blackKing == Piece(Color::black, PieceType::king, 25));
     }
+    SECTION("FEN #2") {
+	const Board b
+		= fen("8/5bp1/B2k4/PP3R1p/3P2p1/P1Kn4/1Q4r1/4B3 w - - 0 1");
+	const auto pl = b.pieceList;
+	REQUIRE(b.whiteToMove);
+	REQUIRE(!b.whiteShort);
+	REQUIRE(!b.blackShort);
+	REQUIRE(!b.whiteLong);
+	REQUIRE(!b.blackLong);
+	REQUIRE(b.enPassantTarget ==  0);
+	REQUIRE(b.halfmoveClock == 0);
+
+	// out of Board
+	for (int i=0; i<10; i++) {
+	    REQUIRE(b.mailbox[i].type       == PieceType::outOfBoard);
+	    REQUIRE(b.mailbox[i + 10].type  == PieceType::outOfBoard);
+	    REQUIRE(b.mailbox[i + 100].type == PieceType::outOfBoard);
+	    REQUIRE(b.mailbox[i + 110].type == PieceType::outOfBoard);
+	}
+	for (int i=2; i<10; i++) {
+	    REQUIRE(b.mailbox[i*10].type   == PieceType::outOfBoard);
+	    REQUIRE(b.mailbox[i*10+9].type == PieceType::outOfBoard);
+	}
+
+	// empty squares
+	REQUIRE(b.mailbox[ 21].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 22].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 23].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 24].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 25].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 26].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 27].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 28].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 31].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 32].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 33].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 34].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 35].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 38].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 42].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 43].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 45].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 46].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 47].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 48].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 53].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 54].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 55].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 57].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 61].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 62].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 63].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 65].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 66].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 68].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 72].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 75].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 76].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 77].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 78].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 81].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 83].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 84].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 85].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 86].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 88].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 91].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 92].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 93].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 94].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 96].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 97].type == PieceType::empty);
+	REQUIRE(b.mailbox[ 98].type == PieceType::empty);
+
+	// pawns
+	REQUIRE(b.mailbox[51].type == PieceType::pawn);
+	REQUIRE(b.mailbox[51].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::pawn, 51)) != pl.end());
+	REQUIRE(b.mailbox[52].type == PieceType::pawn);
+	REQUIRE(b.mailbox[52].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::pawn, 52)) != pl.end());
+	REQUIRE(b.mailbox[64].type == PieceType::pawn);
+	REQUIRE(b.mailbox[64].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::pawn, 64)) != pl.end());
+	REQUIRE(b.mailbox[71].type == PieceType::pawn);
+	REQUIRE(b.mailbox[71].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::pawn, 71)) != pl.end());
+
+	REQUIRE(b.mailbox[37].type == PieceType::pawn);
+	REQUIRE(b.mailbox[37].color == Color::black);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::black, PieceType::pawn, 37)) != pl.end());
+	REQUIRE(b.mailbox[58].type == PieceType::pawn);
+	REQUIRE(b.mailbox[58].color == Color::black);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::black, PieceType::pawn, 58)) != pl.end());
+	REQUIRE(b.mailbox[67].type == PieceType::pawn);
+	REQUIRE(b.mailbox[67].color == Color::black);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::black, PieceType::pawn, 67)) != pl.end());
+
+	// knight(s)
+	REQUIRE(b.mailbox[74].type == PieceType::knight);
+	REQUIRE(b.mailbox[74].color == Color::black);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::black, PieceType::knight, 74)) != pl.end());
+
+	// bishops
+	REQUIRE(b.mailbox[41].type == PieceType::bishop);
+	REQUIRE(b.mailbox[41].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::bishop, 41)) != pl.end());
+	REQUIRE(b.mailbox[95].type == PieceType::bishop);
+	REQUIRE(b.mailbox[95].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::bishop, 95)) != pl.end());
+	REQUIRE(b.mailbox[36].type == PieceType::bishop);
+	REQUIRE(b.mailbox[36].color == Color::black);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::black, PieceType::bishop, 36)) != pl.end());
+
+	// rooks
+	REQUIRE(b.mailbox[56].type == PieceType::rook);
+	REQUIRE(b.mailbox[56].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::rook, 56)) != pl.end());
+	REQUIRE(b.mailbox[87].type == PieceType::rook);
+	REQUIRE(b.mailbox[87].color == Color::black);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::black, PieceType::rook, 87)) != pl.end());
+
+	// queen(s)
+	REQUIRE(b.mailbox[82].type == PieceType::queen);
+	REQUIRE(b.mailbox[82].color == Color::white);
+	REQUIRE(std::find(pl.begin(), pl.end(),
+		    Piece(Color::white, PieceType::queen, 82)) != pl.end());
+
+	// kings
+	REQUIRE(b.mailbox[73].type == PieceType::king);
+	REQUIRE(b.mailbox[44].type == PieceType::king);
+	REQUIRE(b.mailbox[44].color == Color::black);
+	REQUIRE(b.mailbox[73].color == Color::white);
+	REQUIRE(b.whiteKing == Piece(Color::white, PieceType::king, 73));
+	REQUIRE(b.blackKing == Piece(Color::black, PieceType::king, 44));
+    }
 }
